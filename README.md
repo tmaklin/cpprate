5f373dbf715dbb7e056ef6afd37eca055c9fa59d
Tommi Mäklin <tommi@maklin.fi>
2023-04-29 16:18:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# cpprate - Variable Selection in Black Box Methods with RelATive cEntrality (RATE) Measures
+cpprate is a reimplementation of https://github.com/lorinanthony/rate in C++.
+
+# Installation
+## Compiling from source
+### Dependencies
+- cmake >= v3.1
+- C++11 compliant compiler
+- git >= v2.0.0
+
+# Building and running tests
+Build tests with
+```
+mkdir build
+cd build
+cmake -DMCAKE_BUILD_TESTS=1 ..
+make -j
+```
+
+Run tests with
+```
+bin/runTests
+```
+
+# License
+cpprate is licensed under the [BSD-3-Clause license](https://opensource.org/licenses/BSD-3-Clause). A copy of the license is supplied with the project, or can alternatively be obtained from [https://opensource.org/licenses/BSD-3-Clause](https://opensource.org/licenses/BSD-3-Clause).
---
+++ test/src/cpprate_integration_test.cpp
@@ -0,0 +1,67 @@
+// cpprate: Variable Selection in Black Box Methods with RelATive cEntrality (RATE) Measures
+// https://github.com/tmaklin/cpprate
+// Copyright (c) 2023 Tommi Mäklin (tommi@maklin.fi)
+//
+// BSD-3-Clause license
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     (1) Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//     (2) Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//     (3)The name of the author may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+#include "cpprate_integration_test.hpp"
+
+#include "CppRateRes.hpp"
+
+// Test cpprate
+TEST_F(IntegrationTest, EssIsCorrect) {
+    CppRateRes res;
+    EXPECT_NEAR(res.ESS, this->expected_ESS, 1e-7);
+}
+
+// Test cpprate
+TEST_F(IntegrationTest, DeltaIsCorrect) {
+    CppRateRes res;
+    EXPECT_NEAR(res.Delta, this->expected_Delta, 1e-7);
+}
+
+// Test cpprate
+TEST_F(IntegrationTest, RateIsCorrect) {
+    CppRateRes res;
+    for (size_t i = 0; i < n_design_dim; ++i) {
+	EXPECT_NEAR(res.RATE[i], this->expected_RATE[i], 1e-7);
+    }
+
+}
+
+// Test cpprate
+TEST_F(IntegrationTest, KldIsCorrect) {
+    CppRateRes res;
+    for (size_t i = 0; i < n_design_dim; ++i) {
+	EXPECT_EQ(res.KLD[i], this->expected_KLD[i]);
+    }
+
+}
