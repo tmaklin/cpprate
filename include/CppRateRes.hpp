9fd595201beb29b10a4a8fc84bfc23b885c498e0
Tommi Mäklin <tommi@maklin.fi>
2023-05-05 11:42:24 +0100

    Add vec_to_sparse_mat and vec_to_dense_mat; pass Eigen matrices as argument to `RATE` instead of 1d vectors.

--- include/CppRateRes.hpp
+++ include/CppRateRes.hpp
@@ -59,24 +59,6 @@ public:
     }
 };
 
-template <typename T, typename V>
-Eigen::SparseMatrix<T> sparsify_design_matrix(const size_t n_rows, const size_t n_cols, const std::vector<V> &vals) {
-    std::vector<Eigen::Triplet<T>> triplet_list;
-    triplet_list.reserve(n_rows*n_cols);
-
-    for (size_t i = 0; i < n_rows; ++i) {
-	for (size_t j = 0; j < n_cols; ++j) {
-	    if (vals[i*n_cols + j] != (V)0) {
-		T val = (T)vals[i*n_cols + j];
-		triplet_list.emplace_back(Eigen::Triplet<T>(i, j, val));
-	    }
-	}
-    }
-    Eigen::SparseMatrix<T> mat(n_rows, n_cols);
-    mat.setFromTriplets(triplet_list.begin(), triplet_list.end());
-    return mat;
-}
-
 inline Eigen::MatrixXd sherman_r(const Eigen::MatrixXd &ap, const Eigen::VectorXd &u, const Eigen::VectorXd &v) {
     Eigen::MatrixXd nominator = ap * u * Eigen::Transpose<const Eigen::VectorXd>(v) * ap;
     Eigen::MatrixXd denominator = Eigen::MatrixXd::Ones(ap.rows(), ap.cols()) + u * Eigen::Transpose<const Eigen::VectorXd>(v) * ap;
@@ -291,19 +273,41 @@ inline Eigen::VectorXd approximate_beta_means(const Eigen::MatrixXd &f_draws, co
     return v*u*col_means(f_draws);
 }
 
-inline RATEd RATE(const size_t n_obs, const size_t n_snps, const size_t n_f_draws, std::vector<bool> &design_matrix, const std::vector<double> &f_draws, const bool low_rank=false, const size_t low_rank_rank=0) {
+template <typename T>
+inline Eigen::MatrixXd vec_to_dense_matrix(const std::vector<T> &vec, const size_t n_rows, const size_t n_cols) {
+    // TODO tests
+    Eigen::MatrixXd mat(n_rows, n_cols);
+    for (size_t i = 0; i < n_rows; ++i) {
+	for (size_t j = 0; j < n_cols; ++j) {
+	    mat(i, j) = (T)vec[i*n_cols + j];
+	}
+    }
+    return mat;
+}
+
+template <typename T, typename V>
+Eigen::SparseMatrix<T> vec_to_sparse_matrix(const std::vector<V> &vec, const size_t n_rows, const size_t n_cols) {
+    std::vector<Eigen::Triplet<T>> triplet_list;
+    triplet_list.reserve(n_rows*n_cols);
+
+    for (size_t i = 0; i < n_rows; ++i) {
+	for (size_t j = 0; j < n_cols; ++j) {
+	    if (vec[i*n_cols + j] != (V)0) {
+		T val = (T)vec[i*n_cols + j];
+		triplet_list.emplace_back(Eigen::Triplet<T>(i, j, val));
+	    }
+	}
+    }
+    Eigen::SparseMatrix<T> mat(n_rows, n_cols);
+    mat.setFromTriplets(triplet_list.begin(), triplet_list.end());
+    return mat;
+}
+
+inline RATEd RATE(const size_t n_obs, const size_t n_snps, const size_t n_f_draws, const Eigen::SparseMatrix<double> &design_matrix, const Eigen::MatrixXd &f_draws, const bool low_rank=false, const size_t low_rank_rank=0) {
     // ## WARNING: Do not compile with -ffast-math
 
     size_t svd_rank = std::min(n_obs, n_snps);
     const double prop_var = 1.0; // TODO email the authors if this is right (if prop.var == 1.0 the last component is always ignored)?
-
-    const Eigen::SparseMatrix<double> &X = sparsify_design_matrix<double, bool>(n_obs, n_snps, design_matrix);
-    Eigen::MatrixXd f_draws_mat(n_f_draws, n_obs);
-    for (size_t i = 0; i < n_f_draws; ++i) {
-	for (size_t j = 0; j < n_obs; ++j) {
-	    f_draws_mat(i, j) = f_draws[i*n_obs + j];
-	}
-    }
 
     Eigen::MatrixXd cov_beta;
     Eigen::VectorXd col_means_beta;
@@ -311,14 +315,14 @@ inline RATEd RATE(const size_t n_obs, const size_t n_snps, const size_t n_f_draw
     if (low_rank) {
 	Eigen::MatrixXd u;
 	Eigen::MatrixXd v;
-	decompose_design_matrix(X, svd_rank, prop_var, &u, &v);
-	const Eigen::MatrixXd &Sigma_star = project_f_draws(f_draws_mat, u);
+	decompose_design_matrix(design_matrix, svd_rank, prop_var, &u, &v);
+	const Eigen::MatrixXd &Sigma_star = project_f_draws(f_draws, u);
 	const Eigen::MatrixXd &svd_cov_beta_u = decompose_covariance_approximation(Sigma_star, v, low_rank_rank).adjoint(); // This does not work
 	cov_beta = approximate_cov_beta(Sigma_star, v);
-	col_means_beta = approximate_beta_means(f_draws_mat, u, v);
+	col_means_beta = approximate_beta_means(f_draws, u, v);
 	Lambda = create_lambda(svd_cov_beta_u);
     } else {
-	const Eigen::MatrixXd &beta_draws = nonlinear_coefficients(X, f_draws_mat);
+	const Eigen::MatrixXd &beta_draws = nonlinear_coefficients(design_matrix, f_draws);
 	cov_beta = covariance_matrix(beta_draws);
 	const Eigen::MatrixXd &svd_cov_beta_u = decompose_covariance_matrix(cov_beta);
 	col_means_beta = col_means(beta_draws);
